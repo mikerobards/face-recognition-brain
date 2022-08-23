b5c115c6a54e33428dd1dd162d1d27e0c99744aa
Michael Robards <mikerobards@icloud.com>
2022-08-23 17:04:19 -0400

    add Logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo />
+      <Logo />
+      {/*
       <ImageLinkForm />
       <FaceRecognition /> */}
     </div>
