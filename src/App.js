0a953283e328c1ee8aa0a54d005b539722b2c210
Michael Robards <mikerobards@icloud.com>
2022-08-30 13:31:36 -0400

    revert react-scripts to 5.0.1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import Particles from './components/Particles';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
@@ -5,19 +6,40 @@ import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: 'YOUR_API_KEY'
+});
 
-function App() {
 
-  return (
-    <div className="App">
-      <Particles />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputeChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click')
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <Particles />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputeChange={this.onInputeChange} onButtonSubmit={this.onButtonSubmit} />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
+
 
 export default App;
